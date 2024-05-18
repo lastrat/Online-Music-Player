d35c6261f1ecfdbdd0880eb9ae049b8a0b0acc6f
Sim Sim <simosimogildas@gmail.com>
2024-05-18 08:44:28 +0100

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,30 +31,20 @@ android {
 
 dependencies {
 
-    /*implementation ("androidx.appcompat:appcompat:1.0.2")
+    implementation ("androidx.appcompat:appcompat:1.0.2")
     implementation ("androidx.core:core:1.8.0")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.google.firebase:firebase-storage:21.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    //implementation ("de.hdodenhof:circleimageview:2.2.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.firebase:firebase-database:21.0.0")
-    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation ("de.hdodenhof:circleimageview:2.2.0")
     implementation ("com.github.jeancsanchez:JcPlayer:2.7.2")
     implementation("com.squareup.picasso:picasso:2.8")
-    implementation("com.github.bumptech.glide:glide:4.11.0")
-    implementation("de.hdodenhof:circleimageview:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 
 }
